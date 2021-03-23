326c518bd8bd106359302b2d562c651ebf29120f
Youn._ <dbsgmltmd23@naver.com>
2021-03-24 00:13:11 +0900

    Update 주식가격.py
    Add comment

--- Programmers/주식가격.py
+++ Programmers/주식가격.py
@@ -3,7 +3,7 @@
 Level : 2
 """
 
-""" 첫 번째 시도
+""" 1번 코드
 def solution(prices):
     q = []
     answer = [0] * len(prices)
@@ -17,13 +17,14 @@ def solution(prices):
 
     return answer
 """
+# 일단, 효율성은 생각하지 않고 구현했고 효율성테스트에서 탈락하여 쓸데없는 루프를 돌아서 그러는줄 앎
 
-""" 두 번째 시도
+""" 2번 코드
 def solution(prices):
     answer = []
     while prices:
         count = 0
-        value = prices.pop(0) # 아마 이렇게 pop을 하고, element들을 다시 앞으로 당겨주는 과정에서 
+        value = prices.pop(0) # prices.pop(0)을 할때 맨 앞 element를 없애고, 다시 그 자리만큼 정렬을 시켜주니 len(prices)만큼 복잡도 증가
         for item in prices:
             count += 1
             if value > item:
@@ -32,8 +33,9 @@ def solution(prices):
         
     return answer
 """
+# 두번째로 해본 코드인데, 그래도 효율성이 문제가있음 그래서 생각을 해봤더니..
 
-# 세 번째 시도
+# 세 번째 시도 (성공 코드)
 from collections import deque
 
 def solution(prices):
@@ -49,5 +51,7 @@ def solution(prices):
         answer.append(count)
 
     return answer
+
+# 2번 코드의 문제점을 보완하기 위해 deque를 사용, 그래서 맨 앞 요소를 pop해도 재정렬하느라 잡아먹는 시간해결
 
 print(solution([1,2,3,2,3]))
