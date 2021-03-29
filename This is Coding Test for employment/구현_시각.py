6290a3c43a4b8aa690337df951ef200d3ccff79b
gmltmd23 <dbsgmltmd23@naver.com>
2021-03-30 00:04:29 +0900

    test

---
+++ This is Coding Test for employment/구현_시각.py
@@ -0,0 +1,16 @@
+"""
+Part2 구현
+
+시각
+"""
+
+n = int(input())
+
+answer = 0
+for h in range(n + 1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h) + str(m) + str(s):
+                answer += 1
+
+print(answer)
