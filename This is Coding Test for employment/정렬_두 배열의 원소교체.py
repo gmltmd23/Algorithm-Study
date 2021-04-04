cdea3646d37eef8dbaa8c23b6989c7d07a68e5be
unknown <dbsgmltmd23@naver.com>
2021-04-04 16:07:08 +0900

    fix bug

--- This is Coding Test for employment/정렬_두 배열의 원소교체.py
+++ This is Coding Test for employment/정렬_두 배열의 원소교체.py
@@ -2,5 +2,6 @@ n, k = map(int , input().split())
 a = sorted(list(map(int, input().split())))
 b = sorted(list(map(int, input().split())), reverse = True)
 for i in range(k):
-    a[i], b[i] = b[i], a[i]
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
 print(sum(a))
