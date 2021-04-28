7debb46ed9f99a00b6884feba97b94367feaf186
gmltmd23 <dbsgmltmd23@naver.com>
2021-04-28 23:09:32 +0900

    Algorithm Study 2021/04/28 _ 4

---
+++ This is Coding Test for employment/기출문제 풀이/탐색_기출/탐색_정렬된 배열에서 특정 수의 개수 구하기.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+def binary_search(seq, target, start, end):
+    global count
+    if start == end:
+        if seq[start] == target:
+            count += 1
+        return
+    center = (start + end) // 2
+    if target < sequence[center]:
+        return binary_search(seq, target, start, center - 1)
+    elif target > sequence[center]:
+        return binary_search(seq, target, center + 1, end)
+    else:
+        if end - start == 1:
+            center = end
+        binary_search(seq, target, start, center - 1)
+        binary_search(seq, target, center, end)
+
+n, x = map(int, input().split())
+sequence = list(map(int, input().split()))
+count = 0
+binary_search(sequence, x, 0, n - 1)
+print(-1 if count == 0 else count)
