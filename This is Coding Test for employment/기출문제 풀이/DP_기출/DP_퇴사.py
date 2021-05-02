4043160fcd93ec19bf0ead2e573de56fe947c202
unknown <dbsgmltmd23@naver.com>
2021-05-02 16:01:01 +0900

    second modification

--- This is Coding Test for employment/기출문제 풀이/DP_기출/DP_퇴사.py
+++ This is Coding Test for employment/기출문제 풀이/DP_기출/DP_퇴사.py
@@ -1,26 +1,28 @@
+"""
+
+뒤에서부터 보면 더 효율적이게 DP 코드를 짤수가있다.
+답지를 참고하면서 개선을 했는데, 이것저것 고민해보면 이게 가장 효율적인 방법이다.
+
+"""
+
 import sys
-import copy
 input = sys.stdin.readline
 
 n = int(input().rstrip())
 time, pay = [], []
+dp = [0] * (n + 1)
+max_pay = 0
 for i in range(n):
     t, p = map(int, input().split())
     time.append(t)
     pay.append(p)
 
-max_pay = -1
-for i in range(n):
-    now, next = i, i + time[i]
-    temp = copy.deepcopy(pay)
-    while next < n:
-        temp[next] += temp[now]
-        next_time = next + time[next]
-        if next_time > n:
-            next = next_time
-            continue
-        max_pay = max(max_pay, temp[next])
-        now = next
-        next = next_time
+for i in range(n - 1, -1, -1):
+    next = i + time[i]
+    if next <= n:
+        dp[i] = max(max_pay, pay[i] + dp[next])
+        max_pay = dp[i]
+    else:
+        dp[i] = max_pay
 
 print(max_pay)
