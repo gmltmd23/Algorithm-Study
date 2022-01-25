eab91947170710f13a4d0806dd8492f71aef013e
gmltmd23 <dbsgmltmd23@naver.com>
2022-01-25 22:38:40 +0900

    [복습] 그래프 탐색_알고스팟 (2022/01/25)

---
+++ Review/(복습) 그래프 탐색_알고스팟.py
@@ -0,0 +1,36 @@
+"""
+
+(복습) 백준 문제 1261번 그래프 탐색_알고스팟
+
+이 문제의 포인트는 distance라는 2차원 배열을 두는것이고, deque를 사용하는것이다,
+또 graph[nx][ny] == 0 일때는 appendleft로 순서를 당겨서 처리 해주는것이다.
+
+"""
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+graph = []
+for i in range(n):
+    graph.append(list(map(int, list(input().rstrip()))))
+distance = [[-1] * m for _ in range(n)]
+distance[0][0] = 0
+
+q = deque([(0, 0)])
+while q:
+    x, y = q.popleft()
+    for i in range(4):
+        nx, ny = (x + dx[i]), (y + dy[i])
+        if 0 <= nx < n and 0 <= ny < m:
+            if distance[nx][ny] == -1:
+                if graph[nx][ny] == 0:
+                    q.appendleft((nx, ny)) # 이게 제일 중요
+                    distance[nx][ny] = distance[x][y]
+                else:
+                    q.append((nx, ny))
+                    distance[nx][ny] = distance[x][y] + 1
+
+print(distance[n - 1][m - 1])
