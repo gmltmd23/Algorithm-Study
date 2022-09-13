ae60acc1372f778934274c7987075dbe2511f66d
gmltmd23 <dbsgmltmd23@naver.com>
2022-09-13 16:52:01 +0900

    [그리디] 프로그래머스_LEVEL2_N진수 게임 (2022.09.13)

---
+++ 2022 Study/Greedy/[그리디] 프로그래머스_LEVEL2_n진수 게임.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+
+        int num = 0;
+
+        while(sb.length() < m * t) {
+            sb.append(Integer.toString(num++, n));
+        }
+
+        for(int i = p - 1; i < m * t; i = i + m) {
+            answer.append(sb.charAt(i));
+        }
+
+        return answer.toString().toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        int n = 16;
+        int t = 16;
+        int m = 2;
+        int p = 1;
+        System.out.println(solution(n, t, m, p));
+    }
+}
--- 복습필요/[이분탐색] 프로그래머스_LEVEL3_징검다리 건너기.py
+++
@@ -1,7 +0,0 @@
-def solution(stones, k):
-    answer = 0
-    return answer
-
-stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1]
-k = 3
-print(solution(stones, k))
