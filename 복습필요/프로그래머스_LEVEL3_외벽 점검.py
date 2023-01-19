21e159909f9d5042e1a0e8412845a40bd2c1559e
gmltmd23 <dbsgmltmd23@naver.com>
2023-01-19 21:23:45 +0900

    [복습후보] 프로그래머스_LEVEL3_외벽 점검 (2023.01.19)

---
+++ 복습필요/프로그래머스_LEVEL3_외벽 점검.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+
+def solution(n, weak, dist):
+    length = len(weak)
+    for i in range(length):
+        weak.append(weak[i]+n)
+    answer = len(dist) + 1
+
+    for start in range(length):
+        for friends in permutations(dist, len(dist)):
+            count = 1
+            position = weak[start] + friends[count - 1]
+
+            for i in range(start, start + length):
+                if position < weak[i]:
+                    count += 1
+                    if count > len(dist):
+                        break
+                    position = weak[i] + friends[count - 1]
+
+            answer = min(count, answer)
+
+    return -1 if answer > len(dist) else answer
+
+
+n = 12
+weak = [1, 5, 6, 10]
+dist = [1, 2, 3, 4]
+print(solution(n, weak, dist))
